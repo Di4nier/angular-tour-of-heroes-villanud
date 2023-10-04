55365cee180fc52e68a44b767d66e22aac1142d6
Diego Villanueva <diego.villanueva@etu.univ-grenoble-alpes.fr>
2023-10-04 11:20:41 +0200

    Affichage et modification Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Mechanicus'
+  };
+
+  heroes = HEROES;
 
 }
